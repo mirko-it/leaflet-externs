4e0a24f18aec54cc14f2bc7ffd36f08bc9ab0a89
mirko <mirko.xm.miggiano@ericsson.com>
2016-02-02 16:25:44 +0100

    updates

---
+++ leaflet.markercluster.js
@@ -0,0 +1,36 @@
+
+
+/**
+ *  L.MarkerClusterGroup
+**/
+
+
+L.MarkerClusterGroup;
+L.markerClusterGroup;
+
+
+/* Options */
+
+options.zoomToBoundsOnClick;
+options.showCoverageOnHover;
+options.spiderfyOnMaxZoom;
+options.animateAddingMarkers;
+options.disableClusteringAtZoom;
+options.maxClusterRadius;
+options.polygonOptions;
+options.singleMarkerMode;
+options.spiderfyDistanceMultiplier;
+
+
+/* Methods */
+
+layer.zoomToBounds;
+marker.getVisibleParent;
+
+layer.hasLayer;
+layer.zoomToShowLayer;
+layer.addLayers;
+layer.removeLayers;
+
+
+layer.getConvexHull
